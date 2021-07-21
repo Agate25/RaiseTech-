c74e0deed112ae88cfbbe012e1115f434d59424a
masun0168@gmail.com <masun0168@gmail.com>
2021-07-22 06:59:11 +0900

    ハンバーガーメニュー完成

--- RaiseTech/js/hamburger.js
+++ RaiseTech/js/hamburger.js
@@ -1,9 +1,16 @@
+var hamburger = document.querySelector(".js-hamburger");
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("is-open");
+});
+
+const menu = document.querySelector(".")
 
 
 
-// var hamburger =
-// doument.querySelector(".hamburger");
-// hamburger.addEventListener("click",
-// function() {
-//     hamburger.classList.toggle("active");
-// });
+// jQuery(function($) {
+//     $( ".js-humburger" ).on( "click", function() {
+//         $(this).toggleClass("is-open");
+//         $(".p-gmenu--bt").toggleClass("is-open");
+//         $("body").toggleClass("is-open");
+//     } )
+// } );
